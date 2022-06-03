28f3c1c0515a295f5e2132b74b2df53311fb43c6
Savienne <Saviennes.mitchell@yahoo.com>
2022-06-02 23:03:41 -0400

    working ttt

--- js/app.js
+++ js/app.js
@@ -4,21 +4,25 @@ const winningCombos = [[0,1,2], [6,7,8], [3,4,5], [0,3,6], [1,4,7], [2,5,8], [0,
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let turn
+let turn = 1
 let winner
-let board
+let board = [null, null, null, null, null, null, null, null, null]
 
 
 /*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelector(".square")
+const squareEls = document.querySelectorAll(".square")
 const messageEl = document.querySelector("#message")
-// const resetButton = document.querySelector('#reset');
+const resetButton = document.querySelector("#reset")
 
 console.log(squareEls)
 console.log(messageEl)
 /*----------------------------- Event Listeners -----------------------------*/
  
- entireBoard.addEventListener('click', handleClick)
+
+ squareEls.forEach(function(squares) {
+   squares.addEventListener('click',handleClick)
+ })
+// const resetButton = document.querySelector('#reset');
     
 
 
@@ -28,51 +32,51 @@ console.log(messageEl)
 /*-------------------------------- Functions --------------------------------*/
 init()
 function init() {
+    render()
+}
     board =  [null, null, null, null, null, null, null, null, null]
     turn = 1
     winner = null
     render()
-}
+
 
 
 function handleClick(evt){
 
-     const sqIdx = parseInt(evt.target.id.slice(2))
-     console.log(sqIdx)
-     if(board[sqIdx] !== null){
-        return
-     } else if (Winner !== null){
+    let sqIdx = parseInt(evt.target.id.slice(2))
+     if (board[sqIdx] !== null){
+    
+    return
+    
+    } if (winner !== null) {
+    
         return 
-     } else {
-        board[Idx]
-     }
-     board[sqIdx]
-     render()
-     
- }
+    
+    } 
+        board[sqIdx] = turn
+        turn = turn * (-1)
+        render()
+        console.log(board)
+}
+    
+
 
 
 function render() {
     board.forEach((square,idx) => {
-        if (square === 1) {
-            squareEls[idx].textContent = "0"
-
-        } 
-        if (square === -1) {
-            squareEls[idx].textContent = "X"
-        }
-        // } else {
-        //     squareEls[idx].textContent = ''
-        // }
-        if (winner === null) {
-            return (turn === 1 ? messageEl.textContent = "Player 1's turn!!": messageEl.textContent = "Player 2's turn!!")
-        } else if (winner === "T") {
-            return messageEl.textContent = "Tie Game!"
-        } else {
-            return(winner === 1 ? messageEl.textContent = "Player 1 has won!" : messageEl.textContent = "player 2 has won")
-        }
+      if (square === 1) {
+      squareEls[idx].textContent = "0"
+      } else if (square === -1) {
+        squareEls[idx].textContent = "X"
+      } else {
+        squareEls[idx].textContent = ''
+      }
+    })
+    if (winner === null) {
+      return (turn === 1 ? messageEl.textContent = "Player 1's turn!!": messageEl.textContent = "Player 2's turn!!")
+    } else if (winner === "T") {
+      return messageEl.textContent = "Tie Game!"
+    } else {
+      return(winner === 1 ? messageEl.textContent = "Player 1 has won!" : messageEl.textContent = "player 2 has won")
     }
-    )}
-//  function getWinner() {
-//      for (let i = 0; i < winn)
-//  }
+  }
